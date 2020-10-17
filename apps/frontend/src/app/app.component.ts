a6a3b12eef47eaec152e1d8b5113f9d7833c5407
Sharad Shinde <sharad.shinde@coditas.com>
2020-10-17 14:33:22 +0530

    Refactor: Created New Nrwl workspace

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,52 +1,10 @@
-import { Component, ViewContainerRef, OnInit, AfterViewInit } from '@angular/core';
-import { LoaderService } from './core/components/loader/loader.service';
-import { AuthService } from './core/services/auth/auth.service';
-import { SwUpdate } from '@angular/service-worker';
-import {
-  Router, NavigationStart, NavigationCancel, NavigationEnd
-} from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ia-root',
+  selector: 'anstagram-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent implements OnInit, AfterViewInit {
-  loading = true;
-  isUserLoggedIn = false;
-  // tslint:disable-next-line: max-line-length
-  constructor(private loaderService: LoaderService, private vcr: ViewContainerRef, private authService: AuthService, private swUpdate: SwUpdate, private router: Router) {
-    this.loaderService.setViewContainer(vcr);
-  }
-
-  ngOnInit() {
-    this.authService.isLoggedIn.subscribe((loginStatus: any) => {
-      this.isUserLoggedIn = loginStatus;
-    });
-
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.available.subscribe(() => {
-        if (confirm('New version available. Load New Version?')) {
-          window.location.reload();
-        }
-      });
-    }
-  }
-
-  ngAfterViewInit() {
-    this.router.events
-      .subscribe((event) => {
-        if (event instanceof NavigationStart) {
-          this.loading = true;
-        }
-
-        if (
-          event instanceof NavigationEnd ||
-          event instanceof NavigationCancel
-        ) {
-          this.loading = false;
-        }
-      });
-  }
+export class AppComponent {
+  title = 'frontend';
 }
--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://anstagram-api.sharadshinde.in/'
 };
